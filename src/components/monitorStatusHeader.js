ffc275e0af24a3ed8fe69a36032c4215e28a9b2c
Barbara Smith <127714364+rpgmvmz@users.noreply.github.com>
2023-08-16 04:18:50 +0800

    Update monitorStatusHeader.js

--- src/components/monitorStatusHeader.js
+++ src/components/monitorStatusHeader.js
@@ -23,9 +23,9 @@ export default function MonitorStatusHeader({ kvMonitorsLastUpdate }) {
         <div>{text}</div>
         {kvMonitorsLastUpdate.time && typeof window !== 'undefined' && (
           <div className="text-xs font-light">
-            checked{' '}
-            {Math.round((Date.now() - kvMonitorsLastUpdate.time) / 1000)} sec
-            ago (from{' '}
+            {' '}
+            {Math.round((Date.now() - kvMonitorsLastUpdate.time) / 1000)} 秒
+            前检查过了 (来自{' '}20
             {locations[kvMonitorsLastUpdate.loc] || kvMonitorsLastUpdate.loc})
           </div>
         )}
