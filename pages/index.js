7c2f3850e107dabb02193dfbb584fe701fe6cf3b
Barbara Smith <127714364+rpgmvmz@users.noreply.github.com>
2023-08-16 04:36:37 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -71,7 +71,7 @@ export default function Index({ config, kvMonitors, kvMonitorsLastUpdate }) {
         <div className="flex flex-row justify-between items-center p-4">
           <div className="flex flex-row items-center">
             <img className="h-8 w-auto" src={config.settings.logo} />
-            <h1 className="ml-4 text-3xl">{config.settings.title}</h1>
+            <h1 className="ml-4 text-3xl">緒山まひろの状態</h1>
           </div>
           <div className="flex flex-row items-center">
             {typeof window !== 'undefined' && <ThemeSwitcher />}
